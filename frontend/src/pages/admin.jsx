bb4e827042c09b96a5b3e52f1854acf34b94d247
Blue-G <blue-g@Blue-Gs-iMac.local>
2024-08-07 14:14:13 +0400

    fetched food list

--- frontend/src/pages/admin.jsx
+++ frontend/src/pages/admin.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "./sharedComponents/header";
 import Footer from "./sharedComponents/footer";
+import {useState} from "react";
 
 function Admin() {
+    const [food, setFood] = useState([]);
+    let fetchFoodList = async () => {
+        
+        let list = await fetch("http://localhost:4000/api/food/list");
+        let item = await list.json();
+        setFood(item.data);
+        console.log(item);
+            
+       
+    }
+
+    useEffect(()=>{
+        fetchFoodList();
+    }, []);
 
     return (
         <div>
@@ -11,31 +26,27 @@ function Admin() {
             <table class="table">
         <thead class="thead-dark">
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Price</th>
+            <th scope="col">Image</th>
+            <th scope="col">_V</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+            {food.map((item)=> (
+                  <tr>
+                  <th scope="row">{item._id}</th>
+                  <td>{item.name}</td>
+                  <td>{item.description}</td>
+                  <td>{item.price}</td>
+                  <td>{item.image}</td>
+                  <td>{item.__v}</td>
+                </tr>
+            ))}
+        
+       
         </tbody>
       </table>
       </div>
